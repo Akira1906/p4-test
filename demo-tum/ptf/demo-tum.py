5a37ea69ac3b43213fd2fdb775a5b52423aaa2cd
Tristan <tristan.doering@tum.de>
2025-02-25 18:19:13 +0800

    write tests for syn-cookie

--- demo-tum/ptf/demo-tum.py
+++ demo-tum/ptf/demo-tum.py
@@ -1,20 +1,21 @@
-#!/usr/bin/env python3
+# Note: 9559 is the default TCP port number on which the
+# simple_switch_grpc process is listening for incoming TCP connections,
+# over which a client program can send P4Runtime API messages to
+# simple_switch_grpc.
 
-# Copyright 2023 Intel Corporation
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#   http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# p4c --target bmv2 --arch v1model --p4runtime-files proxy.p4info.txtpb proxy.p4
 
-# Andy Fingerhut, andy.fingerhut@gmail.com
+
+#     connection_hash, connection_hash_rev, seq_diff, seq_diff_rev = compute_connection_hash(
+#         src_ip, dst_ip, src_port, dst_port, protocol, cookie_value, seq_no
+#     )
+
+#     print(f"Connection Hash: {connection_hash}")
+#     print(f"Reverse Connection Hash: {connection_hash_rev}")
+#     print(f"Seq Diff: {seq_diff}")
+#     print(f"Seq Diff Rev: {seq_diff_rev}")
+
+
 
 import logging
 
@@ -22,32 +23,14 @@ import ptf
 import ptf.testutils as tu
 from ptf.base_tests import BaseTest
 import p4runtime_sh.shell as sh
-import p4runtime_shell_utils as shu
-
-
-# Links to many Python methods useful when writing automated tests:
-
-# The package `ptf.testutils` contains many useful Python methods for
-# writing automated tests, some of which are demonstrated below with
-# calls prefixed by the local alias `tu.`.  You can see the
-# definitions for all Python code in this package, including some
-# documentation for these methods, here:
-
-# https://github.com/p4lang/ptf/blob/master/src/ptf/testutils.py
+# import p4runtime_shell_utils as shu
+import ipaddress
 
 
 ######################################################################
 # Configure logging
 ######################################################################
 
-# Note: I am not an expert at configuring the Python logging library.
-# Recommendations welcome on improvements here.
-
-# The effect achieved by the code below seems to be that many DEBUG
-# and higher priority logging messages go to the console, and also to
-# a file named 'ptf.log'.  Some of the messages written to the
-# 'ptf.log' file do not go to the console, and appear to be created
-# from within the ptf library.
 
 logger = logging.getLogger(None)
 ch = logging.StreamHandler()
@@ -57,28 +40,31 @@ formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(messag
 ch.setFormatter(formatter)
 logger.addHandler(ch)
 
-# Examples of some kinds of calls that can be made to generate
-# logging messages.
-#logger.debug("10 logger.debug message")
-#logger.info("20 logger.info message")
-#logger.warn("30 logger.warn message")
-#logger.error("40 logger.error message")
-#logging.debug("10 logging.debug message")
-#logging.info("20 logging.info message")
-#logging.warn("30 logging.warn message")
-#logging.error("40 logging.error message")
 
 class DemoTumTest(BaseTest):
-    def __init__():
-        pass
-    # general structure
     
     def setUp(self):
+        # Setting up PTF dataplane
+        self.dataplane = ptf.dataplane_instance
+        self.dataplane.flush()
+
+        logging.debug("DemoTUM.setUp()")
+        grpc_addr = tu.test_param_get("grpcaddr")
+        if grpc_addr is None:
+            grpc_addr = "localhost:9559"
+
+        grpc_addr='0.0.0.0:9559'
+        my_dev1_id=0
+        p4info_txt_fname = tu.test_param_get("p4info")
+        p4prog_binary_fname = tu.test_param_get("config")
+
+        sh.setup(device_id=my_dev1_id,
+                grpc_addr=grpc_addr,
+                election_id=(0, 1), # (high_32bits, lo_32bits)
+                config=sh.FwdPipeConfig(p4info_txt_fname, p4prog_binary_fname),
+                verbose=True)
         # 1. start the P4 program of the tum approach
         # 2. start the control plane python application
-
-        # initatie PTF
-        pass
 
     def tearDown(self):
         logging.debug("DemoTumTest.tearDown()")
@@ -88,171 +74,128 @@ class DemoTumTest(BaseTest):
 
     # but first write some unit tests, artifical but simpler 
 
-class Demo1Test(BaseTest):
-    def setUp(self):
-        # Setting up PTF dataplane
-        self.dataplane = ptf.dataplane_instance
-        self.dataplane.flush()
 
-        logging.debug("Demo1Test.setUp()")
-        grpc_addr = tu.test_param_get("grpcaddr")
-        if grpc_addr is None:
-            grpc_addr = 'localhost:9559'
-        p4info_txt_fname = tu.test_param_get("p4info")
-        p4prog_binary_fname = tu.test_param_get("config")
-        sh.setup(device_id=0,
-                 grpc_addr=grpc_addr,
-                 election_id=(0, 1), # (high_32bits, lo_32bits)
-                 config=sh.FwdPipeConfig(p4info_txt_fname, p4prog_binary_fname),
-                 verbose=False)
-        #shu.dump_table("ipv4_da_lpm")
-        #shu.dump_table("mac_da")
-        #shu.dump_table("send_frame")
+class ProxyTest(DemoTumTest):
+        
 
-    def tearDown(self):
-        logging.debug("Demo1Test.tearDown()")
-        sh.teardown()
-
-#############################################################
-# Define a few small helper functions that make adding entries to
-# particular tables in demo2.p4 slightly shorter to write.
-#############################################################
-
-def add_ipv4_da_lpm_entry_action_set_l2ptr(ipv4_addr_str, prefix_len_int,
-                                           l2ptr_int):
-    te = sh.TableEntry('ipv4_da_lpm')(action='set_l2ptr')
-    # Note: p4runtime-shell raises an exception if you attempt to
-    # explicitly assign to te.match['dstAddr'] a prefix with length 0.
-    # Just skip assigning to te.match['dstAddr'] completely, and then
-    # inserting the entry will give a wildcard match for that field,
-    # as defined in the P4Runtime API spec.
-    if prefix_len_int != 0:
-        te.match['dstAddr'] = '%s/%d' % (ipv4_addr_str, prefix_len_int)
-    te.action['l2ptr'] = '%d' % (l2ptr_int)
-    te.insert()
-
-def add_mac_da_entry_action_set_bd_dmac_intf(l2ptr_int, bd_int, dmac_str,
-                                             intf_int):
-    te = sh.TableEntry('mac_da')(action='set_bd_dmac_intf')
-    te.match['l2ptr'] = '%d' % (l2ptr_int)
-    te.action['bd'] = '%d' % (bd_int)
-    te.action['dmac'] = dmac_str
-    te.action['intf'] = '%d' % (intf_int)
-    te.insert()
-
-def add_send_frame_entry_action_rewrite_mac(out_bd_int, smac_str):
-    te = sh.TableEntry('send_frame')(action='rewrite_mac')
-    te.match['out_bd'] = '%d' % (out_bd_int)
-    te.action['smac'] = smac_str
-    te.insert()
-
-class FwdTest(Demo1Test):
     def runTest(self):
-        in_dmac = 'ee:30:ca:9d:1e:00'
-        in_smac = 'ee:cd:00:7e:70:00'
-        ip_dst_addr = '10.1.0.1'
-        ig_port = 1
+        self.client_mac = "00:00:00:00:01:01"  # h1 MAC
+        self.attacker_mac = "00:00:00:00:02:02"  # h2 MAC
+        self.server_mac = "00:00:00:00:03:03"  # h3 MAC
+        self.switch_mac = "00:aa:bb:cc:dd:ee"  # s1 MAC
 
-        eg_port = 2
-        l2ptr = 58
-        bd = 9
-        out_dmac = '02:13:57:ab:cd:ef'
-        out_smac = '00:11:22:33:44:55'
+        self.client_ip = "10.0.1.1"
+        self.attacker_ip = "10.0.1.2"
+        self.server_ip = "10.0.1.3"
 
-        # Before adding any table entries, the default behavior for
-        # sending in an IPv4 packet is to drop it.
-        pkt = tu.simple_tcp_packet(eth_src=in_smac, eth_dst=in_dmac,
-                                   ip_dst=ip_dst_addr, ip_ttl=64)
-        tu.send_packet(self, ig_port, pkt)
-        tu.verify_no_other_packets(self)
+        self.client_port = 1234
+        self.server_port = 80
+        self.attacker_port = 5555
 
-        # Add a set of table entries that the packet should match, and
-        # be forwarded out with the desired dest and source MAC
-        # addresses.
-        add_ipv4_da_lpm_entry_action_set_l2ptr(ip_dst_addr, 32, l2ptr)
-        add_mac_da_entry_action_set_bd_dmac_intf(l2ptr, bd, out_dmac, eg_port)
-        add_send_frame_entry_action_rewrite_mac(bd, out_smac)
+        self.client_iface = 1  # h1 -> s1
+        self.attacker_iface = 2  # h2 -> s1
+        self.server_iface = 3  # h3 -> s1
+        
+        self.tcp_handshake()
 
-        # Check that the entry is hit, expected source and dest MAC
-        # have been written into output packet, TTL has been
-        # decremented, and that no other packets are received.
-        exp_pkt = tu.simple_tcp_packet(eth_src=out_smac, eth_dst=out_dmac,
-                                       ip_dst=ip_dst_addr, ip_ttl=63)
-        tu.send_packet(self, ig_port, pkt)
-        tu.verify_packets(self, exp_pkt, [eg_port])
+    def tcp_handshake(self):
+        """ Simulates a proper TCP handshake between client and web server """
+        print("\n[INFO] Sending TCP Handshake Packets...")
 
+        # Step 1: SYN
+        syn_pkt = tu.simple_tcp_packet(
+            eth_src=self.client_mac, eth_dst=self.switch_mac,
+            ip_src=self.client_ip, ip_dst=self.server_ip,
+            tcp_sport=self.client_port, tcp_dport=self.server_port,
+            tcp_flags="S"
+        )
+        tu.send_packet(self, self.client_iface, syn_pkt)
 
-class PrefixLen0Test(Demo1Test):
-    def runTest(self):
-        in_dmac = 'ee:30:ca:9d:1e:00'
-        in_smac = 'ee:cd:00:7e:70:00'
-        ig_port = 1
+        # Step 2: SYN-ACK (Server -> Client)
+        syn_ack_pkt = tu.simple_tcp_packet(
+            eth_src=self.server_mac, eth_dst=self.switch_mac,
+            ip_src=self.server_ip, ip_dst=self.client_ip,
+            tcp_sport=self.server_port, tcp_dport=self.client_port,
+            tcp_flags="SA"
+        )
+        tu.send_packet(self, self.server_iface, syn_ack_pkt)
 
-        entries = []
-        # 'ip_dst_addr' and 'prefix_len' fields represent the key to
-        # add to the LPM table.  'pkt_in_dst_addr' is one IPv4 address
-        # such that if a packet is sent in with that as the dest
-        # address, it should match the given table entry, not one of
-        # the others.  There may be many other such addresses, but we
-        # just need one for this particular test.
-        entries.append({'ip_dst_addr': '10.1.0.1',
-                        'prefix_len': 32,
-                        'pkt_in_dst_addr': '10.1.0.1',
-                        'eg_port': 2,
-                        'l2ptr': 58,
-                        'bd': 9,
-                        'out_dmac': '02:13:57:ab:cd:ef',
-                        'out_smac': '00:11:22:33:44:55'})
-        entries.append({'ip_dst_addr': '10.1.0.0',
-                        'prefix_len': 16,
-                        'pkt_in_dst_addr': '10.1.2.3',
-                        'eg_port': 3,
-                        'l2ptr': 59,
-                        'bd': 10,
-                        'out_dmac': '02:13:57:ab:cd:f0',
-                        'out_smac': '00:11:22:33:44:56'})
-        entries.append({'ip_dst_addr': '0.0.0.0',
-                        'prefix_len': 0,
-                        'pkt_in_dst_addr': '20.0.0.1',
-                        'eg_port': 4,
-                        'l2ptr': 60,
-                        'bd': 11,
-                        'out_dmac': '02:13:57:ab:cd:f1',
-                        'out_smac': '00:11:22:33:44:57'})
+        # Step 3: ACK (Client -> Server)
+        ack_pkt = tu.simple_tcp_packet(
+            eth_src=self.client_mac, eth_dst=self.switch_mac,
+            ip_src=self.client_ip, ip_dst=self.server_ip,
+            tcp_sport=self.client_port, tcp_dport=self.server_port,
+            tcp_flags="A"
+        )
+        tu.send_packet(self, self.client_iface, ack_pkt)
 
-        for e in entries:
-            add_ipv4_da_lpm_entry_action_set_l2ptr(e['ip_dst_addr'],
-                                                   e['prefix_len'], e['l2ptr'])
-            add_mac_da_entry_action_set_bd_dmac_intf(e['l2ptr'], e['bd'],
-                                                     e['out_dmac'],
-                                                     e['eg_port'])
-            add_send_frame_entry_action_rewrite_mac(e['bd'], e['out_smac'])
+    def malicious_packets(self):
+        """ Sends malicious packets from the attacker to the web server """
+        print("\n[INFO] Sending Malicious Packets from Attacker...")
 
-        ttl_in = 100
-        for e in entries:
-            ip_dst_addr = e['pkt_in_dst_addr']
-            eg_port = e['eg_port']
-            pkt_in = tu.simple_tcp_packet(eth_src=in_smac, eth_dst=in_dmac,
-                                          ip_dst=ip_dst_addr, ip_ttl=ttl_in)
-            exp_pkt = tu.simple_tcp_packet(eth_src=e['out_smac'],
-                                           eth_dst=e['out_dmac'],
-                                           ip_dst=ip_dst_addr,
-                                           ip_ttl=ttl_in - 1)
-            tu.send_packet(self, ig_port, pkt_in)
-            tu.verify_packets(self, exp_pkt, [eg_port])
-            # Vary TTL in for each packet tested, just to make them
-            # easy to distinguish from each other.
-            ttl_in = ttl_in - 10
+        # Malicious SYN flood attack
+        for i in range(5):
+            syn_flood_pkt = tu.simple_tcp_packet(
+                eth_src=self.attacker_mac, eth_dst=self.switch_mac,
+                ip_src=self.attacker_ip, ip_dst=self.server_ip,
+                tcp_sport=self.attacker_port + i, tcp_dport=self.server_port,
+                tcp_flags="S", tcp_seq=1000 + i
+            )
+            tu.send_packet(self, self.attacker_iface, syn_flood_pkt)
 
+        # Spoofed TCP packets
+        for i in range(3):
+            spoofed_pkt = tu.simple_tcp_packet(
+                eth_src=self.attacker_mac, eth_dst=self.switch_mac,
+                ip_src="1.2.3.4", ip_dst=self.server_ip,  # Fake IP
+                tcp_sport=6666, tcp_dport=self.server_port,
+                tcp_flags="PA", tcp_seq=2000 + i, tcp_ack=999
+            )
+            tu.send_packet(self, self.attacker_iface, spoofed_pkt)
 
-class DupEntryTest(Demo1Test):
-    def runTest(self):
-        ip_dst_addr = '10.0.0.1'
-        l2ptr = 58
+    def valid_packet_sequence(self):
+        """ Sends a short series of valid packets from the client """
+        print("\n[INFO] Sending Valid Data Packets from Client...")
 
-        def add_entry_once():
-            add_ipv4_da_lpm_entry_action_set_l2ptr(ip_dst_addr, 32, l2ptr)
+        for i in range(3):
+            data_pkt = tu.simple_tcp_packet(
+                eth_src=self.client_mac, eth_dst=self.switch_mac,
+                ip_src=self.client_ip, ip_dst=self.server_ip,
+                tcp_sport=self.client_port, tcp_dport=self.server_port,
+                tcp_flags="PA", tcp_seq=1100 + i, tcp_ack=5100,
+                tcp_payload="GET / HTTP/1.1\r\nHost: server\r\n\r\n"
+            )
+            tu.send_packet(self, self.client_iface, data_pkt)
 
-        add_entry_once()
-        with shu.assertP4RuntimeError(self):
-            add_entry_once()
+# class WhitelistingTest(DemoTumTest):
+#     def runTest(self):
+#         in_dmac = 'ee:30:ca:9d:1e:00'
+#         in_smac = 'ee:cd:00:7e:70:00'
+#         ip_dst_addr = '10.1.0.1'
+#         ip_src_addr = '192.168.0.37'
+#         ig_port = 1
+
+#         eg_port = 2
+#         out_dmac = '02:13:57:ab:cd:ef'
+#         out_smac = '00:11:22:33:44:55'
+
+#         # Before adding any table entries, the default behavior for
+#         # sending in an IPv4 packet is to drop it.
+#         pkt = tu.simple_tcp_packet(eth_src=in_smac, eth_dst=in_dmac,
+#                                    ip_dst=ip_dst_addr, ip_src = ip_src_addr,
+#                                      ip_ttl=64)
+#         tu.send_packet(self, ig_port, pkt)
+#         tu.verify_no_other_packets(self)
+
+#         # Add a set of table entries that the packet should match, and
+#         # be forwarded out with the desired dest and source MAC
+#         # addresses.
+#         add_whitelist_entry_action_noaction(ip_src_addr)
+
+#         # Check that the entry is hit, expected source and dest MAC
+#         # have been written into output packet, TTL has been
+#         # decremented, and that no other packets are received.
+#         exp_pkt = tu.simple_tcp_packet(eth_src=out_smac, eth_dst=out_dmac,
+#                                        ip_dst=ip_dst_addr, ip_src=ip_src_addr, ip_ttl=63)
+#         tu.send_packet(self, ig_port, pkt)
+#         tu.verify_packets(self, exp_pkt, [eg_port])
---
+++ demo-tum/syn-cookie_ptf.sh
@@ -0,0 +1,92 @@
+#! /bin/bash
+# Copyright 2021 Andy Fingerhut
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# SPDX-License-Identifier: Apache-2.0
+
+
+# T="`realpath ../testlib`"
+# if [ x"${PYTHONPATH}" == "x" ]
+# then
+#     P="${T}"
+# else
+#     P="${T}:${PYTHONPATH}"
+# fi
+
+P='/home/tristan/p4dev-python-venv/bin/python'
+
+echo "P is: $P"
+
+# Only show a list of tests
+#ptf --pypath "$P" --test-dir ptf --list
+#exit 0
+
+set -x
+p4c --target bmv2 \
+    --arch v1model \
+    --p4runtime-files syn-cookie/p4src/proxy.p4info.txtpb \
+    syn-cookie/p4src/proxy.p4
+    -o syn-cookie/p4src/proxy.json
+
+# Remove any log file written in an earlier run, otherwise
+# simple_switch_grpc will append the new log messages to the end of
+# the existing file.
+/bin/rm -f ss-log.txt
+
+sudo simple_switch_grpc \
+     --log-file syn-cookie_ss-log \
+     --log-flush \
+     --dump-packet-data 10000 \
+     -i 0@veth0 \
+     -i 1@veth2 \
+     -i 2@veth4 \
+     -i 3@veth6 \
+     -i 4@veth8 \
+     -i 5@veth10 \
+     -i 6@veth12 \
+     -i 7@veth14 \
+     --no-p4 &
+echo ""
+echo "Started simple_switch_grpc.  Waiting 2 seconds before starting PTF test ..."
+sleep 2
+
+# Note that the mapping between switch port number and Linux interface
+# names is best to make it correspond with those given when starting
+# the simple_switch_grpc process.  The `ptf` process has no other way
+# of getting this mapping other than by telling it on its command
+# line.
+# source /home/tristan/p4dev-python-venv/bin/activate
+
+sudo -E ${P4_EXTRA_SUDO_OPTS} $(which ptf) \
+    --pypath "$P" \
+    -i 0@veth1 \
+    -i 1@veth3 \
+    -i 2@veth5 \
+    -i 3@veth7 \
+    -i 4@veth9 \
+    -i 5@veth11 \
+    -i 6@veth13 \
+    -i 7@veth15 \
+    --test-params="grpcaddr='localhost:9559';p4info='syn-cookie/p4src/proxy.p4info.txtpb';config='syn-cookie/p4src/proxy.json'" \
+    --test-dir ptf
+
+
+echo ""
+echo "PTF test finished.  Waiting 2 seconds before killing simple_switch_grpc ..."
+sleep 2
+sudo pkill --signal 9 --list-name simple_switch
+echo ""
+echo "Verifying that there are no simple_switch_grpc processes running any longer in 4 seconds ..."
+sleep 4
+ps axguwww | grep simple_switch
